873652cc0f0f878842b9984b9ad8d497a5f45c06
Nicolas Marra <68780542+nicolasmarra@users.noreply.github.com>
2022-10-08 15:10:58 +0200

    add page bio and gallery

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const fullImgBox = document.getElementById('full_img_box')
 const fullImg = document.getElementById('full_img')
 
+const footer = document.getElementById('footer')
+const main = document.getElementById('photos')
+const menu = document.getElementById('menu')
+const open_menu = document.getElementById('open_menu')
+
 function CloseImgBox() {
   fullImgBox.style.display = 'none'
 }
@@ -8,4 +13,20 @@ function CloseImgBox() {
 function OpenFullImg(reference) {
   fullImgBox.style.display = 'flex'
   fullImg.src = reference
+}
+
+function OnMenu() {
+  if (open_menu.classList.contains('close')) {
+    main.style.display = 'block'
+    footer.style.display = 'block'
+    menu.style.display = 'none'
+    open_menu.classList.remove('close')
+    open_menu.classList.add('open')
+  } else {
+    main.style.display = 'none'
+    footer.style.display = 'none'
+    menu.style.display = 'flex'
+    open_menu.classList.remove('open')
+    open_menu.classList.add('close')
+  }
 }
